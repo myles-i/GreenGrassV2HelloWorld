530ff80754ae137176bd4a40b77f0fb4595d9cc8
Myles <myles.iribarne@gmail.com>
2024-09-30 15:10:09 -0300

    use sdk message format and fix message_handler

--- src/main.py
+++ src/main.py
@@ -3,13 +3,14 @@ import json
 import time
 import logging
 from awsgreengrasspubsubsdk.pubsub_client import AwsGreengrassPubSubSdkClient
+from awsgreengrasspubsubsdk.message_formatter import PubSubMessageFormatter
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def message_handler(self, protocol, topic, message_id, status, route, message):
-    logger.info(f"Received message on {topic}: {message}")
+def message_handler(protocol, topic, message_id, status, route, message_payload):
+    logger.info(f"Received message on {topic}: {message_payload}")
 
 
 if __name__ == "__main__":
@@ -17,18 +18,24 @@ if __name__ == "__main__":
     subscribe_topic = "GGHelloWorld/inbox"
     publish_topic = "GGHelloWorld/outbox"
 
-    # Initialize MQTT client
+    # Initialize client (both mqtt and ipc pubsub - i.e. "ipc_mqtt")
     client = AwsGreengrassPubSubSdkClient(base_topic=base_topic, default_message_handler=message_handler)
     client.activate_mqtt_pubsub()
+    client.activate_ipc_pubsub()
         
-    # Subscribe to the MQTT topic
-    client.subscribe_to_topic("mqtt", subscribe_topic)
+    # Subscribe to topic
+    client.subscribe_to_topic("ipc_mqtt", subscribe_topic)
+
+    # use sdk message formatter for consistent messaging API (starting point for routes etc
+    message_formatter = PubSubMessageFormatter()
 
     try:
         while True:
-            message = {"message": "Hello World, from GreenGrassCore V2"}
-            client.publish_message("mqtt", message, topic=publish_topic)  # Publish using MQTT protocol
-            logger.info(f"Published hello world message to {publish_topic}: {message}")
+            my_message = {"user_msg": "Hello World, from GreenGrassCore V2"}
+            sdk_message = message_formatter.get_message(message=my_message)
+            client.publish_message('ipc_mqtt', sdk_message, topic=publish_topic)  # Publish using MQTT and IPC protocol
+            
+            logger.info(f"Published sdk message to {publish_topic}: {sdk_message}")
             time.sleep(5)
     except Exception as e:
         logger.error(f"Error running the component: {e}")
